d4b83c1d739347046d914f3da1bd9f618ec97165
NotTheCatto <129914663+NotTheCatto@users.noreply.github.com>
2023-10-31 13:12:27 +0800

    Add toggle function and CSS for each task

--- to-do-list.js
+++ to-do-list.js
@@ -7,15 +7,26 @@ function addTask() {
     var td2 = document.createElement("td");
     var td3 = document.createElement("td");
 
-    td1.innerHTML = '<span onclick="this.innerHTML == &quot;&#9989;&quot; ? this.innerHTML = &quot;&#10060;&quot; : this.innerHTML = &quot;&#9989;&quot;">&#10060;</span>';
+    td1.innerHTML = '<span onclick="toggleStatus(this, this.parentElement.parentElement)">&#10060;</span>';
     td2.innerHTML = inputText;
     td3.innerHTML = '<button onclick="this.parentElement.parentElement.innerText = &quot;&quot;">Remove</button>';
     
     tRow.appendChild(td1);
     tRow.appendChild(td2);
     tRow.appendChild(td3);
+    tRow.className = "statusPending";
 
     document.getElementById("taskList").appendChild(tRow);
 
     document.getElementById("taskInput").value = '';
+}
+
+function toggleStatus(el, parentEl){
+    if (parentEl.className == 'statusComplete') {
+        el.innerHTML = "&#10060;";
+        parentEl.className = ('statusPending');
+    } else {
+        el.innerHTML = "&#9989;";
+        parentEl.className = ('statusComplete');
+    }
 }
